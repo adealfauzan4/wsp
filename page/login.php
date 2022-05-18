2d1b50381e3d99379a1fa7c82abe28ef575661df
ade4173 <ade.al.fauzan@gmail.com>
2022-05-18 13:20:34 +0700

    Initial commit

---
+++ page/login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../style/style.css">
+    <title>WSP Backdoor</title>
+</head>
+<body>
+    <div class="header">
+        <div class="header_logo">
+            <a href="http://www.wsp.co.id">
+                <img class="logo_header" src="https://wsp.co.id/wp-content/uploads/2018/12/Logo-WSP-new-e1544597590513.png" alt="">
+            </a>
+        </div>
+        <div class="language">
+            <div class="lang id"></div>
+            <div class="lang us"></div>
+        </div>
+
+    </div>
+    <div class="main">
+
+    </div>
+    <div class="footer">
+
+    </div>
+</body>
+</html>
